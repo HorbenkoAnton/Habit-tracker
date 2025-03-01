bec4bf12898367ad49b18af5c12419c1b36c28fb
HorbenkoAnton <gorbenko.anton.punk2077@gmail.com>
2025-03-01 14:17:55 +0100

    Initial commit

---
+++ habit_tracker.py
@@ -0,0 +1,16 @@
+#program idea
+#Simple GUI habit tracker app 
+#Functional reqs:
+# 1. You can choose your predicted productivity lvl for every day ( for start 3 lvls )
+# 2. You can add habits to track and track them every day ( for start yes\no )
+# 3. All data must be saved between program working sessions
+# 4. Program must have a simple easy and functional GUI 
+
+
+# Good to have:
+# 1. You can set quantity of predicted productivity lvl
+# 2. You can set marks for habits and mark em or choose to have yes\no
+# 3. Maybe you can have some relation of predicted productivity lvl and habit marks
+# 4. Weekly habit stats
+# ( i realy want to implement something like that something that would collect and analyse some data) 
+# ( Maybe data sience or AI is really option, but for now i'll stick to the plan )
